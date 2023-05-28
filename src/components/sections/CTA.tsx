87bc359d75255d8df45eec54d60cf2609d88085a
Markus Wedler <markuswedler8@gmail.com>
2023-05-28 15:27:55 +1000

    Update CTA section

--- src/components/sections/CTA.tsx
+++ src/components/sections/CTA.tsx
@@ -4,6 +4,7 @@ import { ctaType } from "../../Types"
 const CTA = ({ cta }: { cta: ctaType | undefined }) => {
   return (
     <section className="cta wrapper">
+    <div className="img" style={{ backgroundImage: "url('/assets/images/neighbors.svg')" }}/>
       <h1>{ cta && cta.title }</h1>
       <a href="mailto:ghbdtnghbdtn8@gmail.com" className="btn contact">{ cta && cta.contact }</a>
     </section>
